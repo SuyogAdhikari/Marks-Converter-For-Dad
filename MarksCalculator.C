e3c4c52983dea7e63396ce1b9cefa04265a2061d
forty4minutes <39236988+forty4minutes@users.noreply.github.com>
2020-01-08 06:26:54 +0545

    Update MarksCalculator.C

--- MarksCalculator.C
+++ MarksCalculator.C
@@ -7,25 +7,33 @@ int main()
     float number;
     float  InitialPercent, Obtained;
     int InitialMarks, FinalMarks;
+	int FinalFullMarks, FinalFullMarks;
     
-    printf("\nEnter Initial Marks : ");
+    printf("Enter Initial Full Marks : ");
+    scanf("%d",&InitialFullMarks);
+
+    printf("\nEnter Initial Obtained Marks : ");
     scanf("%d",&InitialMarks);
     
-    printf("Enter Final Marks : ");
+    printf("Enter Final Full Marks : ");
+    scanf("%d",&FinalFullMarks);
+
+    printf("Enter Final Obtained Marks : ");
     scanf("%d",&FinalMarks);
+
     
     while(1)
     {   
         printf("\n-------------------------------------------");
         printf("\nEnter marks in %d :",InitialMarks);
         scanf("%f",&number);
-        InitialPercent = number/75;
+        InitialPercent = number/InitialFullMarks;
         InitialPercent = InitialPercent*100;
         
         printf("\nPercent in %d is : %f",InitialMarks, InitialPercent);
         
         Obtained=(InitialPercent/100);
-        Obtained = Obtained * 60;
+        Obtained = Obtained * FinalFullMarks;
         printf("\nMarks in %d is : %f",FinalMarks, Obtained);
 	}
 }
