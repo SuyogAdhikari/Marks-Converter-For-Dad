60deb25ec3610062dc6ab7c437a346cd47758af0
forty4minutes <39236988+forty4minutes@users.noreply.github.com>
2020-01-08 21:42:54 +0545

    error fixed

--- MarksCalculator.C
+++ MarksCalculator.C
@@ -6,34 +6,28 @@ int main()
 {
     float number;
     float  InitialPercent, Obtained;
-    int InitialMarks, FinalMarks;
-	int InitialFullMarks, FinalFullMarks;
+    int InitialFullMarks, FinalFullMarks;
     
     printf("Enter Initial Full Marks : ");
     scanf("%d",&InitialFullMarks);
 
-    printf("\nEnter Initial Obtained Marks : ");
-    scanf("%d",&InitialMarks);
-    
+   
     printf("Enter Final Full Marks : ");
     scanf("%d",&FinalFullMarks);
-
-    printf("Enter Final Obtained Marks : ");
-    scanf("%d",&FinalMarks);
 
     
     while(1)
     {   
         printf("\n-------------------------------------------");
-        printf("\nEnter marks in %d :",InitialMarks);
+        printf("\nEnter marks in %d :",InitialFullMarks);
         scanf("%f",&number);
         InitialPercent = number/InitialFullMarks;
         InitialPercent = InitialPercent*100;
         
-        printf("\nPercent in %d is : %f",InitialMarks, InitialPercent);
+        printf("\nPercent in %d is : %f",InitialFullMarks, InitialPercent);
         
         Obtained=(InitialPercent/100);
         Obtained = Obtained * FinalFullMarks;
-        printf("\nMarks in %d is : %f",FinalMarks, Obtained);
+        printf("\nMarks in %d is : %f",FinalFullMarks, Obtained);
 	}
 }
